bf85ba2b6edcb27cd495406cd9535a342b9a2beb
Wolfgang Keller <wolfgangkeller@gmail.com>
2012-05-10 00:04:07 +0200

    Initial version.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CooperativeSokoban
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void FileOpenLevel_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            dlg.DefaultExt = "*.xsb";
+            dlg.Filter = "Hexoban level|*.hsb|XSokoban/SokoMind Plus level|*.xsb|Txt levels|*.txt|All files|*.*";
+            bool? result = dlg.ShowDialog();
+
+            if (result == true)
+            {
+                // Open document
+                string filename = dlg.FileName;
+            }
+        }
+    }
+}
